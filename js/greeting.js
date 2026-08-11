e9593f234a0d3bd6a7e9d8afde8daec9f62fc2cf
nimbus-sudo <lesszero.here@gmail.com>
2026-08-11 21:48:07 +0530

    fixed greetings

--- js/greeting.js
+++ js/greeting.js
@@ -2,10 +2,10 @@ function getGreeting() {
   const hour = new Date().getHours();
   const name = "Nimbus-sudo";
 
-  if (hour >= 5 && hour < 12) return `$ echo "Good morning"`;
-  if (hour >= 12 && hour < 17) return `$ echo "Good afternoon"`;
-  if (hour >= 17 && hour < 21) return `$ echo "Good evening"`;
-  if (hour >= 21 && hour < 24) return `$ echo "Good night"`;
+  if (hour >= 5 && hour < 12) return `Good morning.`;
+  if (hour >= 12 && hour < 17) return `Good afternoon.`;
+  if (hour >= 17 && hour < 21) return `Good evening.`;
+  if (hour >= 21 && hour < 24) return `Good night.`;
   return `$ echo "You're still here? Respect."`; // midnight/night owl
 }
 
