229eb1decb1c71f44fc9340818ae4a363da1c689
nimbus-sudo <lesszero.here@gmail.com>
2026-08-15 03:14:12 +0530

    greeting screen rework and added about me section with profile img

--- js/greeting.js
+++ js/greeting.js
@@ -6,7 +6,7 @@ function getGreeting() {
   if (hour >= 12 && hour < 17) return `Good afternoon.`;
   if (hour >= 17 && hour < 21) return `Good evening.`;
   if (hour >= 21 && hour < 24) return `Good night.`;
-  return `$ echo "You're still here? Respect."`; // midnight/night owl
+  return "You're still here? Respect."; // midnight/night owl
 }
 
 document.getElementById("greeting").textContent = getGreeting();
